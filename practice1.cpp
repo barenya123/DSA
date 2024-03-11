cdad0091084f534ee298a0958dd010e2e8837299
barenya123 <153221836+barenya123@users.noreply.github.com>
2024-03-11 10:54:57 +0530

    Add files via upload

---
+++ practice1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int Main(){
+    int n;
+    cout<<"Enter the size of the array";
+    cin>>n;
+    int arr[n];
+    cout<<"The size of the array";
+    for(int i = 0;i<n;i++){
+         
+    }
+}
